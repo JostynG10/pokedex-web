25dd97c0c9c2d343e6ad25df7e55925b32eb04e9
JostynG10 <jostyngomez5@gmail.com>
2025-03-29 22:33:38 -0500

    Completed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,27 @@ import styles from "@/styles/Header.module.css";
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.headerForm}>
+      <nav className={styles.headerForm}>
         <div className={styles.leftHeaderForm} />
         <div className={styles.middleHeaderForm} />
         <div className={styles.rightHeaderForm} />
-      </div>
+      </nav>
+
+      <section className={styles.content}>
+        <div className={styles.borderSphere}>
+          <span className={styles.sphere} />
+        </div>
+
+        <div className={styles.textAndPoints}>
+          <ul className={styles.pointsBox}>
+            <li className={`${styles.point} ${styles.pointRed}`} />
+            <li className={`${styles.point} ${styles.pointYellow}`} />
+            <li className={`${styles.point} ${styles.pointGreen}`} />
+          </ul>
+
+          <h1 className={styles.title}>Pokédex</h1>
+        </div>
+      </section>
     </header>
   );
 };
