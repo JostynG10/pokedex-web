bd8e2d2ccade883e3e5f9bf99573a55fb3c0e178
jostyng10 <jostyngomez5@gmail.com>
2025-03-31 11:00:55 -0500

    Little corrections to HomeButton component

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -2,19 +2,23 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { setCurrentOffset } from "@/store/slices/paginationSlice";
 import styles from "@/styles/HomeButton.module.css";
 
 const HomeButton: React.FC = () => {
+  const dispatch = useDispatch();
   const router = useRouter();
 
   const handleClick = () => {
     router.push("/");
+    dispatch(setCurrentOffset(0));
   };
 
   return (
-    <div onClick={handleClick} className={styles.borderSphere}>
+    <button type="button" onClick={handleClick} className={styles.borderSphere}>
       <span className={styles.sphere} />
-    </div>
+    </button>
   );
 };
 
